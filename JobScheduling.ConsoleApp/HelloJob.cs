09fb9dbfdf6c70bbd5fc07aae9bb3b4a02dc9adf
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-26 21:57:15 +0800

    Add project files.

---
+++ JobScheduling.ConsoleApp/HelloJob.cs
@@ -0,0 +1,12 @@
+﻿namespace JobScheduling.ConsoleApp;
+
+using Quartz;
+using System.Threading.Tasks;
+
+public sealed class HelloJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await Console.Out.WriteLineAsync("Hello from HelloJob");
+    }
+}
---
+++ JobScheduling.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using JobScheduling.ConsoleApp;
+using Quartz;
+using Quartz.Impl;
+
+StdSchedulerFactory factory = new();
+IJobDetail job = JobBuilder.Create<HelloJob>()
+    .WithIdentity("job1", "group1")
+    .WithDescription("An example job")
+    .Build();
+IScheduler scheduler = await factory.GetScheduler();
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("trigger1", "group1")
+    .StartNow()
+    .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
+    .Build();
+
+// and start it off
+await scheduler.Start();
+
+await scheduler.ScheduleJob(job, trigger);
+
+// some sleep to show what's happening
+await Task.Delay(TimeSpan.FromSeconds(60));
+
+// and last shut down the scheduler when you are ready to close your program
+await scheduler.Shutdown();
+
