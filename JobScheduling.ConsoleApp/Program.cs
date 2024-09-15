ccc15348071a7e21cda8584d13aa6486247ffc63
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-16 07:47:40 +0800

    refactor: Improve code structure and readability

--- JobScheduling.ConsoleApp/Program.cs
+++ JobScheduling.ConsoleApp/Program.cs
@@ -1,27 +1,50 @@
-﻿using JobScheduling.ConsoleApp;
+﻿namespace JobScheduling.ConsoleApp;
+
 using Quartz;
 using Quartz.Impl;
 
-StdSchedulerFactory factory = new();
-IJobDetail job = JobBuilder.Create<HelloJob>()
-    .WithIdentity("job1", "group1")
-    .WithDescription("An example job")
-    .Build();
-IScheduler scheduler = await factory.GetScheduler();
-ITrigger trigger = TriggerBuilder.Create()
-    .WithIdentity("trigger1", "group1")
-    .StartNow()
-    .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
-    .Build();
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var job = CreateJob();
+        var trigger = CreateTrigger();
+        var scheduler = await CreateSchedulerAsync();
 
-// and start it off
-await scheduler.Start();
+        await scheduler.Start();
+        await scheduler.ScheduleJob(job, trigger);
 
-await scheduler.ScheduleJob(job, trigger);
+        await Console.Out.WriteLineAsync("Press any key to shutdown.");
+        _ = Console.ReadKey();
 
-// some sleep to show what's happening
-await Task.Delay(TimeSpan.FromSeconds(60));
+        await Console.Out.WriteLineAsync("Shutting down...");
+        await scheduler.Shutdown();
+    }
 
-// and last shut down the scheduler when you are ready to close your program
-await scheduler.Shutdown();
+    public static IJobDetail CreateJob()
+    {
+        IJobDetail job = JobBuilder.Create<HelloJob>()
+            .WithIdentity("job1", "group1")
+            .WithDescription("An example job")
+            .Build();
 
+        return job;
+    }
+
+    public static ITrigger CreateTrigger()
+    {
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartNow()
+            .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
+            .Build();
+                
+        return trigger;
+    }
+
+    public static Task<IScheduler> CreateSchedulerAsync()
+    {
+        StdSchedulerFactory factory = new();
+        return factory.GetScheduler();
+    }
+}
