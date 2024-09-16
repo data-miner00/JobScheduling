558ea14dd2b7e772c23f0f94e27afc54b9ebff52
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-16 08:45:45 +0800

    refactor: Separate configuration blocks

--- JobScheduling.WebApiExample/Program.cs
+++ JobScheduling.WebApiExample/Program.cs
@@ -9,27 +9,57 @@ using Quartz.Impl;
 
 public static class Program
 {
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
+
+        builder
+            .ConfigureSwagger()
+            .ConfigureHostedServices()
+            .ConfigureQuartzJobs();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+
+    static WebApplicationBuilder ConfigureSwagger(this WebApplicationBuilder builder)
+    {
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        return builder;
+    }
+
+    static WebApplicationBuilder ConfigureHostedServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHostedService<LogBackgroundService>();
+        builder.Services.AddHostedService<QuartzBackgroundService>();
+
+        return builder;
+    }
+
+    static WebApplicationBuilder ConfigureQuartzJobs(this WebApplicationBuilder builder)
+    {
         builder.Services.Configure<FirstJobOption>(
             builder.Configuration.GetSection(nameof(FirstJobOption)));
 
         builder.Services.Configure<SecondJobOption>(
             builder.Configuration.GetSection(nameof(SecondJobOption)));
 
-        builder.Services.AddHostedService<LogBackgroundService>();
-        builder.Services.AddHostedService<QuartzBackgroundService>();
-
-        var scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+        var scheduler = StdSchedulerFactory.GetDefaultScheduler().GetAwaiter().GetResult();
         builder.Services.AddSingleton(scheduler);
 
         builder.Services.AddSingleton<IEnumerable<JobDescriptor>>(provider => new List<JobDescriptor>
@@ -48,22 +78,6 @@ public static class Program
             },
         });
 
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        return builder;
     }
 }
