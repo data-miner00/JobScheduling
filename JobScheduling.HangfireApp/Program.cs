572f4905c33b338731526ea325ae3c7baed37226
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-16 11:42:49 +0800

    feat: Setup examples

--- JobScheduling.HangfireApp/Program.cs
+++ JobScheduling.HangfireApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hangfire;
+using Hangfire.Storage.SQLite;
+
+GlobalConfiguration.Configuration.UseSQLiteStorage();
+using var server = new BackgroundJobServer();
+Console.WriteLine("Hangfire Started");
+
+var jobId = BackgroundJob.Enqueue(
+    () => Console.WriteLine("Fire-and-forget!"));
+var jobId2 = BackgroundJob.Schedule(
+    () => Console.WriteLine("Delayed!"),
+    TimeSpan.FromDays(7));
+BackgroundJob.ContinueJobWith(
+    jobId,
+    () => Console.WriteLine("Continuation!"));
+
+Console.ReadKey();
+server.SendStop();
